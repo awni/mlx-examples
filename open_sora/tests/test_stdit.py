7163e71de3f9ea8f946464015f5cd3a9319f0325
Awni Hannun <awni@apple.com>
2024-07-27 07:10:27 -0700

    two tests pass

--- open_sora/tests/test_stdit.py
+++ open_sora/tests/test_stdit.py
@@ -1,22 +1,23 @@
 # Copyright © 2024 Apple Inc.
 
-import torch
-import numpy as np
-import mlx.core as mx
-
 import sys
+
+import mlx.core as mx
+import numpy as np
+import torch
+
 sys.path.append(".")
 
-from utils import load_model
 from models import STDiT3
+from utils import load_model
 
 
 def get_inputs():
     np.random.seed(0)
     x = np.random.uniform(size=[2, 8, 30, 40, 4]).astype(np.float32)
-    timestep = np.array([1000., 1000.]).astype(np.float32)
+    timestep = np.array([1000.0, 1000.0]).astype(np.float32)
     mask = np.zeros((1, 20)).astype(np.int32)
-    mask[:10 ] = 1
+    mask[:10] = 1
     x_mask = np.ones((2, 8)).astype(np.bool_)
     y = np.random.uniform(size=[2, 20, 4096])
     height = np.array([240])
@@ -27,21 +28,23 @@ def get_inputs():
 
 def compute_pt(inputs):
     from opensora.models.stdit import stdit3
-    pt_model = stdit3.STDiT3.from_pretrained("hpcai-tech/OpenSora-STDiT-v3")
-    pt_inputs = list(map(torch.tensor, inputs))
-    pt_inputs[0] = pt_inputs[0].moveaxis(4, 1)
-    pt_inputs[2] = pt_inputs[2].unsqueeze(1)
-    pt_out = pt_model(*pt_inputs)
-    return pt_out.moveaxis(1, 4).detach().numpy()
+
+    model = stdit3.STDiT3.from_pretrained("hpcai-tech/OpenSora-STDiT-v3")
+    inputs = list(map(torch.tensor, inputs))
+    inputs[0] = inputs[0].moveaxis(4, 1)
+    inputs[2] = inputs[2].unsqueeze(1)
+    out = model(*inputs)
+    return out.moveaxis(1, 4).detach().numpy()
+
 
 def compute_mx(inputs):
-    # Load converted weights
     model = load_model("mlx_models/OpenSora-STDiT-v3", STDiT3)
     return model(*map(mx.array, inputs))
+
 
 # Compare
 inputs = get_inputs()
 pt_out = compute_pt(inputs)
 mx_out = compute_mx(inputs)
 mx.eval(mx_out)
-np.allclose(mx_out, pt_out, rtol=1e-3, atol=1e-1)
+assert np.allclose(mx_out, pt_out, rtol=1e-3, atol=1e-1)
--- open_sora/tests/test_vae.py
+++ open_sora/tests/test_vae.py
@@ -1,48 +1,70 @@
+# Copyright © 2024 Apple Inc.
 
-import numpy as np
-import mlx.core as mx
 import json
-from huggingface_hub import snapshot_download
+import sys
 from pathlib import Path
 
-import diffusers.models
-from autoencoder_kl import AutoencoderKL
+import mlx.core as mx
+import numpy as np
+import torch
+from huggingface_hub import snapshot_download
 
-def fetch_from_hub(hf_repo: str, patterns=None) -> Path:
-    default_patterns = [ "*.json", "*.safetensors"]
-    model_path = Path(
-        snapshot_download(
-            repo_id=hf_repo,
-            allow_patterns=patterns or default_patterns,
-        )
-    )
-    return model_path
+sys.path.append(".")
+
+from models.autoencoder_kl import AutoencoderKL
+from utils import load_model
 
 hf_repo = "PixArt-alpha/pixart_sigma_sdxlvae_T5_diffusers"
-path = fetch_from_hub(hf_repo, patterns=["vae/*"])
-#print(path)
 
-with open(path / "vae/config.json", 'r') as fid:
-    config = json.load(fid)
-print(config)
-exit(0)
-weights = mx.load(str(path / "vae/diffusion_pytorch_model.safetensors"))
-def torch_to_mlx(key, value):
-    if "conv" in key and value.ndim == 4:
-        value = value.moveaxis(1, 3)
-    return key, value
-weights = [torch_to_mlx(k, v) for k, v in weights.items()]
+
+def fetch_from_hub():
+    patterns = ["vae/*.json", "vae/*.safetensors"]
+    return Path(
+        snapshot_download(
+            repo_id=hf_repo,
+            allow_patterns=patterns,
+        )
+    )
+
+
+def compute_pt(x, y):
+    import diffusers.models
+
+    model = diffusers.models.AutoencoderKL.from_pretrained(hf_repo, subfolder="vae")
+    x, y = map(lambda z: torch.tensor(np.moveaxis(z, 3, 1)), (x, y))
+    y_hat = model.encode(torch.tensor(x)).latent_dist.mean
+    x_hat = model.decode(torch.tensor(y)).sample
+    return map(
+        lambda z: z.moveaxis(1, 3).detach().numpy(),
+        (y_hat, x_hat),
+    )
+
+
+def compute_mx(x, y):
+    path = fetch_from_hub()
+    with open(path / "vae/config.json", "r") as fid:
+        config = json.load(fid)
+
+    weights = mx.load(str(path / "vae/diffusion_pytorch_model.safetensors"))
+
+    def torch_to_mlx(key, value):
+        if "conv" in key and value.ndim == 4:
+            value = value.moveaxis(1, 3)
+        return key, value
+
+    weights = [torch_to_mlx(k, v) for k, v in weights.items()]
+
+    model = AutoencoderKL(**config)
+    model.load_weights(weights)
+    y_hat = model.encode(mx.array(x)).mean
+    x_hat = model.decode(mx.array(y))
+    return y_hat, x_hat
+
 
 np.random.seed(0)
-x = np.random.uniform(size=(2, 4, 32, 32)).astype(np.float32)
-pt_model = diffusers.models.AutoencoderKL.from_pretrained(hf_repo, subfolder="vae")
-import torch
-#pt_y = pt_model.encode(torch.tensor(x))
-pt_y = pt_model.decode(torch.tensor(x))
-#print(pt_y.latent_dist.std)
-#print(pt_y.latent_dist.mean)
-
-model = AutoencoderKL(**config)
-model.load_weights(weights)
-mlx_y = model.encode(mx.array(np.moveaxis(x, 1, 3)))
-mlx_y = model.decode(mx.array(np.moveaxis(x, 1, 3)))
+x = np.random.uniform(size=(2, 32, 32, 3)).astype(np.float32)
+y = np.random.uniform(size=(2, 32, 32, 4)).astype(np.float32)
+pt_y, pt_x = compute_pt(x, y)
+mx_y, mx_x = compute_mx(x, y)
+assert np.allclose(pt_y, mx_y, rtol=1e-4, atol=1e-2)
+assert np.allclose(pt_x, mx_x, rtol=1e-4, atol=1e-3)
