12466135a7df5465dfd1efdc92f51321d812fd2b
Awni Hannun <awni@apple.com>
2024-07-27 10:29:15 -0700

    t5 test

--- open_sora/tests/test_t5.py
+++ open_sora/tests/test_t5.py
@@ -1,53 +1,48 @@
+# Copyright © 2024 Apple Inc.
+
+import sys
+
 import mlx.core as mx
-from transformers import AutoConfig, AutoTokenizer
+import numpy as np
 import torch
-import transformers
-from t5 import T5
+from transformers import AutoTokenizer
 
-hf_path = "DeepFloyd/t5-v1_1-xxl"
+sys.path.append(".")
 
-def load_t5(hf_path):
-    from transformers import T5EncoderModel
-    config = AutoConfig.from_pretrained(hf_path).to_dict()
-    model = T5EncoderModel.from_pretrained(hf_path, torch_dtype="auto")
-    replacements = [
-        (".layer.0.layer_norm.", ".ln1."),
-        (".layer.1.layer_norm.", ".ln2."),
-        (
-            "block.0.layer.0.SelfAttention.relative_attention_bias.",
-            "relative_attention_bias.embeddings.",
-        ),
-        (".layer.0.SelfAttention.", ".attention."),
-        (".layer.1.DenseReluDense.", ".dense."),
-    ]
-    def replace(k):
-        for o, n in replacements:
-            k = k.replace(o, n)
-        return k
+from models import T5
+from utils import load_model
 
-    weights = [
-        (replace(k), mx.array(v)) for k, v in model.state_dict().items()
-    ]
-    tokenizer = AutoTokenizer.from_pretrained(hf_path)
-    model = T5(**config)
-    model.load_weights(weights)
-    return model, tokenizer, config
 
-model, tokenizer, _ = load_t5(hf_path)
+def compute_pt(text):
+    from opensora.models.text_encoder import T5Encoder
+
+    model = T5Encoder("DeepFloyd/t5-v1_1-xxl", device="mps")
+    out = model.encode(text)
+    y, mask = out["y"], out["mask"]
+    y = y.squeeze(0)
+    return map(lambda x: x.cpu().detach().numpy(), (y, mask))
+
+
+def compute_mx(text):
+    text_encoder = load_model("mlx_models/t5-v1_1-xxl", T5)
+    tokenizer = AutoTokenizer.from_pretrained("mlx_models/t5-v1_1-xxl")
+    inputs = tokenizer(
+        text,
+        max_length=120,
+        padding="max_length",
+        truncation=True,
+        return_attention_mask=True,
+        add_special_tokens=True,
+        return_tensors="mlx",
+    )
+    input_ids = inputs["input_ids"]
+    mask = inputs["attention_mask"]
+    y = text_encoder.encode(input_ids, mask)
+    return y, mask
+
+
 text = "a beautiful waterfall"
-from opensora.models.text_encoder import T5Encoder
-pt_model = T5Encoder(from_pretrained=hf_path, device="mps")
-pt_out = pt_model.encode(text)
-print(pt_out)
-
-text_tokens_and_mask = tokenizer(
-    text,
-    max_length=120,
-    padding="max_length",
-    truncation=True,
-    return_attention_mask=True,
-    add_special_tokens=True,
-    return_tensors="mlx",
-)
-out = model.encode(text_tokens_and_mask["input_ids"])
-print(out)
+pt_y, pt_mask = compute_pt(text)
+mx_y, mx_mask = compute_mx(text)
+assert np.allclose(pt_y.squeeze(0), mx_y, rtol=1e-4, atol=1e-1)
+assert np.allclose(pt_mask, mx_mask)
